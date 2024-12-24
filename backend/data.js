182a6c210fe6ce63a94a356c003b70ba0e6e893a
Suxrob <orifovsuxrov@gmail.com>
2024-12-24 22:47:54 +0500

    gfgf

---
+++ backend/data.js
@@ -0,0 +1,27 @@
+const miniBarMenu = [
+  {
+    id: 1,
+    name: "40 AH",
+    images: "",
+  },
+  {
+    id: 2,
+    name: "50 AH",
+    images: "",
+  },
+  {
+    id: 3,
+    name: "60 AH",
+    images: "",
+  },
+  {
+    id: 4,
+    name: "70 AH",
+    images: "",
+  },
+  {
+    id: 5,
+    name: "90 AH",
+    images: "",
+  },
+];
